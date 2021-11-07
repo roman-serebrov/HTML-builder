89a4ae1095f35f8c36114f824e3be2b7de6431d3
Roman Serebrov <romanspb00@mail.ru>
2021-11-07 19:42:14 +0300

    add: copy-folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const readdir = require('fs/promises');
+const dirPath = path.join(`${__dirname}/files-copy`);
 
+fs.access(dirPath, fs.F_OK, (err) => {
+  if (err) {
+    copyFolder();
+  } else {
+    fs.promises.rmdir(dirPath, { recursive: true })
+      .then(() => {
+        console.log('directory removed!');
+        copyFolder();
+      });
+  }
+});
+
+
+
+
+function copyFolder() {
+  fs.promises.mkdir(dirPath, { recursive: true }).then(() => {
+    readdir.readdir(`${__dirname}/files`).then(files => {
+      for(let file of files) {
+        fs.copyFile(`${__dirname}/files/${file}`,`${__dirname}/files-copy/${file}`, err => {
+          if(err) throw err;
+          console.log(`Файл ${file} перенесен`);
+        });
+      }
+    });
+  });
+}
